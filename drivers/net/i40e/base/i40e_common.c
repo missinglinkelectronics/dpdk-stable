277b4d5b1847e9cc429e549bac69862c5682f102
Qi Zhang <qi.z.zhang@intel.com>
2018-01-09 15:30:13 -0500

    net/i40e/base: fix link LED blink
    [ upstream commit e35eed328516f90c51bd41b9ac6623a3e5951549 ]
    
    This fix solves an issue occurring while calling i40e_led_set function
    from the driver with "blink" parameter set as TRUE. This call resulted
    in Activity LED blinking instead of Link LED, which may lead to errors
    in physically identyfying the port, since Activity LED may be blinking
    for different reasons aswell.
    
    Fixes: 51b27bc5d898 ("i40e/base: update LED blinking")
    
    Signed-off-by: Qi Zhang <qi.z.zhang@intel.com>
    Acked-by: Beilei Xing <beilei.xing@intel.com>

--- drivers/net/i40e/base/i40e_common.c
+++ drivers/net/i40e/base/i40e_common.c
@@ -1579,6 +1579,7 @@ u32 i40e_led_get(struct i40e_hw *hw)
 		case I40E_COMBINED_ACTIVITY:
 		case I40E_FILTER_ACTIVITY:
 		case I40E_MAC_ACTIVITY:
+		case I40E_LINK_ACTIVITY:
 			continue;
 		default:
 			break;
@@ -1627,6 +1628,7 @@ void i40e_led_set(struct i40e_hw *hw, u32 mode, bool blink)
 		case I40E_COMBINED_ACTIVITY:
 		case I40E_FILTER_ACTIVITY:
 		case I40E_MAC_ACTIVITY:
+		case I40E_LINK_ACTIVITY:
 			continue;
 		default:
 			break;
@@ -1636,9 +1638,6 @@ void i40e_led_set(struct i40e_hw *hw, u32 mode, bool blink)
 		/* this & is a bit of paranoia, but serves as a range check */
 		gpio_val |= ((mode << I40E_GLGEN_GPIO_CTL_LED_MODE_SHIFT) &
 			     I40E_GLGEN_GPIO_CTL_LED_MODE_MASK);
-
-		if (mode == I40E_LINK_ACTIVITY)
-			blink = false;
 
 		if (blink)
 			gpio_val |= BIT(I40E_GLGEN_GPIO_CTL_LED_BLINK_SHIFT);
