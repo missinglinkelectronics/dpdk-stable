1a17e82544bc36b604a53b48a6212e86c0c03460
Shahaf Shuler <shahafs@mellanox.com>
2018-01-22 22:52:14 +0200

    net/mlx5: fix missing RSS capability
    [ upstream commit b233b027490b858f740a75fac19632a25d3aefdd ]
    
    The PMD was not reporting the supported RSS capabilities.
    
    Fixes: 2f97422e7759 ("mlx5: support RSS hash update and get")
    
    Signed-off-by: Shahaf Shuler <shahafs@mellanox.com>
    Acked-by: Yongseok Koh <yskoh@mellanox.com>

--- drivers/net/mlx5/mlx5_ethdev.c
+++ drivers/net/mlx5/mlx5_ethdev.c
@@ -658,6 +658,7 @@ mlx5_dev_infos_get(struct rte_eth_dev *dev, struct rte_eth_dev_info *info)
 			       (*priv->rss_conf)[0]->rss_key_len :
 			       0);
 	info->speed_capa = priv->link_speed_capa;
+	info->flow_type_rss_offloads = ~MLX5_RSS_HF_MASK;
 	priv_unlock(priv);
 }
 
