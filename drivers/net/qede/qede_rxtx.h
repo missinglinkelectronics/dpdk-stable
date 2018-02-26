622d15e9a75f52be37869c94af3d8682000da201
Rasesh Mody <rasesh.mody@cavium.com>
2018-02-25 21:16:12 -0800

    net/qede: fix MTU set and max Rx pkt len usage
    [ backported from upstream commit 9e334305178fd3715c17088632544bf58e5836a9 ]
    [ backported from upstream commit 1ef4c3a5c1f76b810620b76d82a31ce33a83d3fe ]
    [ backported from upstream commit f6033f2497e7d01d8c6f0d691e2f86937597aa35 ]
    
    This patch fixes issues related to MTU set and max_rx_pkt_len usage.
     - Adjust MTU during device configuration when jumbo is enabled
    
     - In qede_set_mtu():
       Return not supported for VF as currently we do not support it.
    
       Add check for RXQ allocation before calculating RX buffer size
       if not allocated defer RX buffer size calculation till RXQ setup.
    
       Add check for before performing device start/stop.
    
     - Use max_rx_pkt_len appropriately
    
     - Change QEDE_ETH_OVERHEAD macro to adjust driver specifics
    
     -  The driver can handle dynamic MTU change without needing the port to be
        stopped explicitly by the application. However, there is currently no
        check to prevent I/Os from happening on a different thread while the
        port is going thru' reset internally. This patch fixes this issue by
        assigning RX/TX burst functions to a dummy function and also reconfigure
        RX bufsize for each rx queue based on the new MTU value.
    
     - Fix minimum RX buffer size to 1024B
    
     - Force enable scatter/gather mode if given RX buf size is lesser than MTU
    
     - Adjust RX buffer size to cache-line size with overhead included
    
    Fixes: bec0228816c0 ("net/qede: support scatter gather")
    Fixes: 2ea6f76aff40 ("qede: add core driver")
    
    Signed-off-by: Harish Patil <harish.patil@cavium.com>
    Signed-off-by: Rasesh Mody <rasesh.mody@cavium.com>

--- drivers/net/qede/qede_rxtx.h
+++ drivers/net/qede/qede_rxtx.h
@@ -55,14 +55,21 @@
 	((flags) & (PARSING_AND_ERR_FLAGS_TUNNEL8021QTAGEXIST_MASK \
 		<< PARSING_AND_ERR_FLAGS_TUNNEL8021QTAGEXIST_SHIFT))
 
+#define QEDE_MIN_RX_BUFF_SIZE		(1024)
+#define QEDE_VLAN_TAG_SIZE		(4)
+#define QEDE_LLC_SNAP_HDR_LEN		(8)
+
 /* Max supported alignment is 256 (8 shift)
  * minimal alignment shift 6 is optimal for 57xxx HW performance
  */
 #define QEDE_L1_CACHE_SHIFT	6
 #define QEDE_RX_ALIGN_SHIFT	(RTE_MAX(6, RTE_MIN(8, QEDE_L1_CACHE_SHIFT)))
 #define QEDE_FW_RX_ALIGN_END	(1UL << QEDE_RX_ALIGN_SHIFT)
-
-#define QEDE_ETH_OVERHEAD       (ETHER_HDR_LEN + 8 + 8 + QEDE_FW_RX_ALIGN_END)
+#define QEDE_CEIL_TO_CACHE_LINE_SIZE(n) (((n) + (QEDE_FW_RX_ALIGN_END - 1)) & \
+					~(QEDE_FW_RX_ALIGN_END - 1))
+/* Note: QEDE_LLC_SNAP_HDR_LEN is optional */
+#define QEDE_ETH_OVERHEAD	(((2 * QEDE_VLAN_TAG_SIZE)) - (ETHER_CRC_LEN) \
+				+ (QEDE_LLC_SNAP_HDR_LEN))
 
 /* TBD: Excluding IPV6 */
 #define QEDE_RSS_OFFLOAD_ALL    (ETH_RSS_IPV4 | ETH_RSS_NONFRAG_IPV4_TCP | \
@@ -179,6 +186,10 @@ uint16_t qede_xmit_pkts(void *p_txq, struct rte_mbuf **tx_pkts,
 
 uint16_t qede_recv_pkts(void *p_rxq, struct rte_mbuf **rx_pkts,
 			uint16_t nb_pkts);
+
+uint16_t qede_rxtx_pkts_dummy(__rte_unused void *p_rxq,
+			      __rte_unused struct rte_mbuf **pkts,
+			      __rte_unused uint16_t nb_pkts);
 
 /* Fastpath resource alloc/dealloc helpers */
 int qede_alloc_fp_resc(struct qede_dev *qdev);
