982600ace70b6ac6e308e9f50e42e967487183a8
Chas Williams <chas3@att.com>
2018-03-17 21:45:52 -0400

    net/vmxnet3: keep link state consistent
    [ upstream commit 3a14b29bff9cc427920c83391b37cededa48fd2f ]
    
    The vmxnet3 never attempts link speed negotiation.  As a virtual device
    the link speed is vague at best.  However, it is important for certain
    applications, like bonding, to see a consistent link_status.  802.3ad
    requires that only links of the same cost (link speed) be enslaved.
    Keeping the link status consistent in vmxnet3 avoids races with bonding
    enslavement.
    
    Fixes: 1e3a958f40b3 ("ethdev: fix link autonegotiation value")
    
    Signed-off-by: Chas Williams <chas3@att.com>
    Acked-by: Yong Wang <yongwang@vmware.com>

--- drivers/net/vmxnet3/vmxnet3_ethdev.c
+++ drivers/net/vmxnet3/vmxnet3_ethdev.c
@@ -794,7 +794,7 @@ vmxnet3_dev_link_update(struct rte_eth_dev *dev,
 		link.link_status = ETH_LINK_UP;
 		link.link_duplex = ETH_LINK_FULL_DUPLEX;
 		link.link_speed = ETH_SPEED_NUM_10G;
-		link.link_autoneg = ETH_LINK_AUTONEG;
+		link.link_autoneg = ETH_LINK_FIXED;
 	}
 
 	vmxnet3_dev_atomic_write_link_status(dev, &link);
