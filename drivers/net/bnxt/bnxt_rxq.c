05777b5e744a9ac45f24760aefb4de701fe0010e
Ajit Khaparde <ajit.khaparde@broadcom.com>
2018-04-16 18:11:25 -0700

    net/bnxt: avoid freeing memzone multiple times
    [ upstream commit 23460b4c6a7ab68e2b6d217460684a554f7e78eb ]
    
    Since we are storing the mem_zone address for each ring created,
    we are freeing the same address multiple times.
    For example the memory zone created for Rx is being freed during
    Rx ring cleanup, AGG ring cleanup and CQ cleanup.
    Avoid this by storing the memory zone address in RXQ instead and
    free it as a part of queue_release dev_op.
    In the same way do the same for TX queues as well.
    
    Fixes: 51c87ebafc7d ("net/bnxt: add Tx queue create/destroy")
    
    Signed-off-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

--- drivers/net/bnxt/bnxt_rxq.c
+++ drivers/net/bnxt/bnxt_rxq.c
@@ -256,6 +256,8 @@ void bnxt_rx_queue_release_op(void *rx_queue)
 		bnxt_free_ring(rxq->cp_ring->cp_ring_struct);
 
 		bnxt_free_rxq_stats(rxq);
+		rte_memzone_free(rxq->mz);
+		rxq->mz = NULL;
 
 		rte_free(rxq);
 	}
@@ -306,7 +308,7 @@ int bnxt_rx_queue_setup_op(struct rte_eth_dev *eth_dev,
 
 	eth_dev->data->rx_queues[queue_idx] = rxq;
 	/* Allocate RX ring hardware descriptors */
-	if (bnxt_alloc_rings(bp, queue_idx, NULL, rxq->rx_ring, rxq->cp_ring,
+	if (bnxt_alloc_rings(bp, queue_idx, NULL, rxq, rxq->cp_ring,
 			"rxr")) {
 		RTE_LOG(ERR, PMD, "ring_dma_zone_reserve for rx_ring failed!");
 		bnxt_rx_queue_release_op(rxq);
