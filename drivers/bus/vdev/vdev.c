956163d6c7879ae3acacef4fc727028d03bd3554
Stephen Hemminger <stephen@networkplumber.org>
2018-05-16 14:51:40 -0700

    bus/vdev: fix double space in logs
    [ upstream commit 999951c86bcb95b3c72a193a15f01692045544fc ]
    
    The VDEV_LOG() macro already adds a newline, don't duplicate.
    
    Fixes: d22fcb225c24 ("bus/vdev: change log type")
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- drivers/bus/vdev/vdev.c
+++ drivers/bus/vdev/vdev.c
@@ -165,7 +165,7 @@ vdev_probe_all_drivers(struct rte_vdev_device *dev)
 
 	name = rte_vdev_device_name(dev);
 
-	VDEV_LOG(DEBUG, "Search driver %s to probe device %s\n", name,
+	VDEV_LOG(DEBUG, "Search driver %s to probe device %s", name,
 		rte_vdev_device_name(dev));
 
 	if (vdev_parse(name, &driver))
@@ -275,7 +275,7 @@ vdev_remove_driver(struct rte_vdev_device *dev)
 	const struct rte_vdev_driver *driver;
 
 	if (!dev->device.driver) {
-		VDEV_LOG(DEBUG, "no driver attach to device %s\n", name);
+		VDEV_LOG(DEBUG, "no driver attach to device %s", name);
 		return 1;
 	}
 
@@ -373,7 +373,7 @@ vdev_probe(void)
 			continue;
 
 		if (vdev_probe_all_drivers(dev)) {
-			VDEV_LOG(ERR, "failed to initialize %s device\n",
+			VDEV_LOG(ERR, "failed to initialize %s device",
 				rte_vdev_device_name(dev));
 			ret = -1;
 		}
