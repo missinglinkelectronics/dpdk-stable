229c9d10dd292189b07e2a737f6cfda33963dcdd
Yanglong Wu <yanglong.wu@intel.com>
2018-11-13 14:34:43 +0800

    net/ixgbevf: fix link state
    [ upstream commit 91546fb62e675943a574c2542c0486f2928fed49 ]
    
    For ixgbevf kernel driver, link status changes from down to up
    will trigger vf kernel driver send IXGBE_VF_RESET message to pf
    kernel driver, after this, vf kernel driver will disable and enable
    it self. By these series operations, the vf kernel driver report
    link up. Besides, all these operations handles in kernel thread.
    For DPDK user space driver, it only gets link status changes from
    down to up, but miss IXGBE_VF_RESET message sending and reset itself.
    If we will add fully implementation of link status change for DPDK
    user space driver, we need take much more modification. We have
    aligned that for link status changes from down to up we only notify
    link is up, users need to reset vf port.
    
    Fixes: dc66e5fd01b9 ("net/ixgbe: improve link state check on VF")
    
    Signed-off-by: Rosen Xu <rosen.xu@intel.com>
    Signed-off-by: Yanglong Wu <yanglong.wu@intel.com>
    Acked-by: Qi Zhang <qi.z.zhang@intel.com>

--- drivers/net/ixgbe/ixgbe_ethdev.c
+++ drivers/net/ixgbe/ixgbe_ethdev.c
@@ -3227,13 +3227,8 @@ ixgbevf_dev_info_get(struct rte_eth_dev *dev,
 
 static int
 ixgbevf_check_link(struct ixgbe_hw *hw, ixgbe_link_speed *speed,
-		   int *link_up, int wait_to_complete)
+		   int *link_up, int wait_to_complete __rte_unused)
 {
-	/**
-	 * for a quick link status checking, wait_to_compelet == 0,
-	 * skip PF link status checking
-	 */
-	bool no_pflink_check = wait_to_complete == 0;
 	struct ixgbe_mbx_info *mbx = &hw->mbx;
 	struct ixgbe_mac_info *mac = &hw->mac;
 	uint32_t links_reg, in_msg;
@@ -3294,14 +3289,6 @@ ixgbevf_check_link(struct ixgbe_hw *hw, ixgbe_link_speed *speed,
 		*speed = IXGBE_LINK_SPEED_UNKNOWN;
 	}
 
-	if (no_pflink_check) {
-		if (*speed == IXGBE_LINK_SPEED_UNKNOWN)
-			mac->get_link_status = true;
-		else
-			mac->get_link_status = false;
-
-		goto out;
-	}
 	/* if the read failed it could just be a mailbox collision, best wait
 	 * until we are called again and don't report an error
 	 */
@@ -3311,7 +3298,7 @@ ixgbevf_check_link(struct ixgbe_hw *hw, ixgbe_link_speed *speed,
 	if (!(in_msg & IXGBE_VT_MSGTYPE_CTS)) {
 		/* msg is not CTS and is NACK we must have lost CTS status */
 		if (in_msg & IXGBE_VT_MSGTYPE_NACK)
-			ret_val = -1;
+			mac->get_link_status = false;
 		goto out;
 	}
 
