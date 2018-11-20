959bee3ddbf0ebec8248dd7fd4e62f4a37aedc0b
Yanglong Wu <yanglong.wu@intel.com>
2018-11-20 13:59:21 +0800

    net/ixgbe: fix TDH register write
    [ upstream commit ff30a020bb400f04bf64a91416993862f45f9ada ]
    
    The only time that software should write to the TDH register
    is after a reset (hardware reset or CTRL.RST) and
    before enabling the transmit function (TXDCTL.ENABLE).
    If software were to write to this register while the transmit
    function was enabled, the on-chip descriptor buffers might
    be invalidated and the hardware could become confused.
    
    Fixes: 029fd06d40fa ("ixgbe: queue start and stop")
    
    Signed-off-by: Yanglong Wu <yanglong.wu@intel.com>
    Acked-by: Qi Zhang <qi.z.zhang@intel.com>

--- drivers/net/ixgbe/ixgbe_rxtx.c
+++ drivers/net/ixgbe/ixgbe_rxtx.c
@@ -5000,6 +5000,7 @@ ixgbe_dev_tx_queue_start(struct rte_eth_dev *dev, uint16_t tx_queue_id)
 
 	if (tx_queue_id < dev->data->nb_tx_queues) {
 		txq = dev->data->tx_queues[tx_queue_id];
+		IXGBE_WRITE_REG(hw, IXGBE_TDH(txq->reg_idx), 0);
 		txdctl = IXGBE_READ_REG(hw, IXGBE_TXDCTL(txq->reg_idx));
 		txdctl |= IXGBE_TXDCTL_ENABLE;
 		IXGBE_WRITE_REG(hw, IXGBE_TXDCTL(txq->reg_idx), txdctl);
@@ -5017,7 +5018,6 @@ ixgbe_dev_tx_queue_start(struct rte_eth_dev *dev, uint16_t tx_queue_id)
 					     "Tx Queue %d", tx_queue_id);
 		}
 		rte_wmb();
-		IXGBE_WRITE_REG(hw, IXGBE_TDH(txq->reg_idx), 0);
 		IXGBE_WRITE_REG(hw, IXGBE_TDT(txq->reg_idx), 0);
 		dev->data->tx_queue_state[tx_queue_id] = RTE_ETH_QUEUE_STATE_STARTED;
 	} else
