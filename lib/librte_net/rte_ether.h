196a99100cec6bdd9ba444811d9fb66ae8ff0b15
Bill Hong <bhong@brocade.com>
2019-03-25 11:05:41 -0400

    net: fix Tx VLAN flag for offload emulation
    [ backported from upstream commit 9ea1b3ccfcfd556ec5144fa91041cf8f86814c5f ]
    
    A PMD might use rte_vlan_insert to implement Tx VLAN offload. Typically
    the PMD will insert the VLAN header in the transmit path and then
    attempt to send the packets. If this fails, the packets are returned to
    the application which may attempt to send these packets again. If the
    PKT_TX_VLAN flag is not cleared, the transmit path may attempt to insert
    the VLAN header again.
    
    Fixes: 47aa48b969f8 ("net: fix stripped VLAN flag for offload emulation")
    
    Signed-off-by: Bill Hong <bhong@brocade.com>
    Signed-off-by: Chas Williams <chas3@att.com>
    Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- lib/librte_net/rte_ether.h
+++ lib/librte_net/rte_ether.h
@@ -409,7 +409,7 @@ static inline int rte_vlan_insert(struct rte_mbuf **m)
 	vh = (struct vlan_hdr *) (nh + 1);
 	vh->vlan_tci = rte_cpu_to_be_16((*m)->vlan_tci);
 
-	(*m)->ol_flags &= ~PKT_RX_VLAN_STRIPPED;
+	(*m)->ol_flags &= ~(PKT_RX_VLAN_STRIPPED | PKT_TX_VLAN_PKT);
 
 	return 0;
 }
