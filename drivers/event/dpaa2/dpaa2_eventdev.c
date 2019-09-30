a769f8e3be198aa1a2322136fe2be605209a73e3
Hemant Agrawal <hemant.agrawal@nxp.com>
2019-09-30 14:02:10 +0530

    event/dpaa2: fix default queue configuration
    [ upstream commit 6f213fe94002cd446e919ecc79715656a2657a50 ]
    
    Test vector expect only one type of scheduling as default.
    The old code is provide support scheduling types instead of default.
    
    Fixes: 13370a3877a5 ("eventdev: fix inconsistency in queue config")
    
    Signed-off-by: Hemant Agrawal <hemant.agrawal@nxp.com>

--- drivers/event/dpaa2/dpaa2_eventdev.c
+++ drivers/event/dpaa2/dpaa2_eventdev.c
@@ -1,7 +1,7 @@
 /*-
  *   BSD LICENSE
  *
- *   Copyright 2017 NXP.
+ *   Copyright 2017,2019 NXP.
  *
  *   Redistribution and use in source and binary forms, with or without
  *   modification, are permitted provided that the following conditions
@@ -395,8 +395,7 @@ dpaa2_eventdev_queue_def_conf(struct rte_eventdev *dev, uint8_t queue_id,
 	RTE_SET_USED(queue_conf);
 
 	queue_conf->nb_atomic_flows = DPAA2_EVENT_QUEUE_ATOMIC_FLOWS;
-	queue_conf->schedule_type = RTE_SCHED_TYPE_ATOMIC |
-				      RTE_SCHED_TYPE_PARALLEL;
+	queue_conf->schedule_type = RTE_SCHED_TYPE_PARALLEL;
 	queue_conf->priority = RTE_EVENT_DEV_PRIORITY_NORMAL;
 }
 
