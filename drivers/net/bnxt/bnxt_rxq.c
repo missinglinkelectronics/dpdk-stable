54eac7bf5e69e162f228cb3dc7235a744c24d2f4
Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
2019-10-09 18:41:53 -0700

    net/bnxt: remove unnecessary variable assignment
    [ upstream commit cd30e6a7239ce48c8b618c87b10ee5db094d9853 ]
    
    There is no need to assign return value to a temporary variable.
    Instead return error directly in case of failure.
    
    Fixes: 1fe427fd08ee ("net/bnxt: support enable/disable interrupt")
    
    Signed-off-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Signed-off-by: Ajit Khaparde <ajit.khaparde@broadcom.com>
    Reviewed-by: Santoshkumar Karanappa Rastapur <santosh.rastapur@broadcom.com>

--- drivers/net/bnxt/bnxt_rxq.c
+++ drivers/net/bnxt/bnxt_rxq.c
@@ -371,10 +371,9 @@ bnxt_rx_queue_intr_enable_op(struct rte_eth_dev *eth_dev, uint16_t queue_id)
 
 	if (eth_dev->data->rx_queues) {
 		rxq = eth_dev->data->rx_queues[queue_id];
-		if (!rxq) {
-			rc = -EINVAL;
-			return rc;
-		}
+		if (!rxq)
+			return -EINVAL;
+
 		cpr = rxq->cp_ring;
 		B_CP_DB_ARM(cpr);
 	}
@@ -390,10 +389,9 @@ bnxt_rx_queue_intr_disable_op(struct rte_eth_dev *eth_dev, uint16_t queue_id)
 
 	if (eth_dev->data->rx_queues) {
 		rxq = eth_dev->data->rx_queues[queue_id];
-		if (!rxq) {
-			rc = -EINVAL;
-			return rc;
-		}
+		if (!rxq)
+			return -EINVAL;
+
 		cpr = rxq->cp_ring;
 		B_CP_DB_DISARM(cpr);
 	}
