368b63ca65b313dde4e85d85ea5a926914c4b207
Ciara Power <ciara.power@intel.com>
2020-02-14 16:17:25 +0000

    lib: fix unnecessary double negation
    [ upstream commit f42c9ac5b6b5fabd3c6bb124914af6704445e853 ]
    
    An equality expression already returns either 0 or 1.
    There is no need to use double negation for these cases.
    
    Fixes: ea672a8b1655 ("mbuf: remove the rte_pktmbuf structure")
    Fixes: a0fd91cefcc0 ("mempool: rename functions with confusing names")
    
    Signed-off-by: Ciara Power <ciara.power@intel.com>
    Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>
    Acked-by: Olivier Matz <olivier.matz@6wind.com>

--- lib/librte_mempool/rte_mempool.h
+++ lib/librte_mempool/rte_mempool.h
@@ -1573,7 +1573,7 @@ rte_mempool_in_use_count(const struct rte_mempool *mp);
 static inline int
 rte_mempool_full(const struct rte_mempool *mp)
 {
-	return !!(rte_mempool_avail_count(mp) == mp->size);
+	return rte_mempool_avail_count(mp) == mp->size;
 }
 
 /**
@@ -1592,7 +1592,7 @@ rte_mempool_full(const struct rte_mempool *mp)
 static inline int
 rte_mempool_empty(const struct rte_mempool *mp)
 {
-	return !!(rte_mempool_avail_count(mp) == 0);
+	return rte_mempool_avail_count(mp) == 0;
 }
 
 /**
