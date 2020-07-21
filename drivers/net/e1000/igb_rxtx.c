73571bbc3128689eef159341fcba7bddd1fa8a2a
Zhihong Peng <zhihongx.peng@intel.com>
2020-07-20 23:05:14 -0400

    net/e1000: report VLAN extend capability
    [ upstream commit 21f4a1fb30ffd566769f9a4be8775433c00c44d0 ]
    
    The rte_eth_dev_set_vlan_offload function will check vlan rx offload
    capability, the i350/i210/i211 nics have vlan extend feature but
    DEV_RX_OFFLOAD_VLAN_EXTEND is not set into the capability, that will
    cause setting fail. So need to add this capability in
    igb_get_rx_port_offloads_capa function.
    
    Fixes: ef990fb56e55 ("net/e1000: convert to new Rx offloads API")
    
    Signed-off-by: Zhihong Peng <zhihongx.peng@intel.com>
    Reviewed-by: Wei Zhao <wei.zhao1@intel.com>

--- drivers/net/e1000/igb_rxtx.c
+++ drivers/net/e1000/igb_rxtx.c
@@ -1634,8 +1634,10 @@ uint64_t
 igb_get_rx_port_offloads_capa(struct rte_eth_dev *dev)
 {
 	uint64_t rx_offload_capa;
+	struct e1000_hw *hw;
 
-	RTE_SET_USED(dev);
+	hw = E1000_DEV_PRIVATE_TO_HW(dev->data->dev_private);
+
 	rx_offload_capa = DEV_RX_OFFLOAD_VLAN_STRIP  |
 			  DEV_RX_OFFLOAD_VLAN_FILTER |
 			  DEV_RX_OFFLOAD_IPV4_CKSUM  |
@@ -1644,6 +1646,11 @@ igb_get_rx_port_offloads_capa(struct rte_eth_dev *dev)
 			  DEV_RX_OFFLOAD_JUMBO_FRAME |
 			  DEV_RX_OFFLOAD_KEEP_CRC    |
 			  DEV_RX_OFFLOAD_SCATTER;
+
+	if (hw->mac.type == e1000_i350 ||
+	    hw->mac.type == e1000_i210 ||
+	    hw->mac.type == e1000_i211)
+		rx_offload_capa |= DEV_RX_OFFLOAD_VLAN_EXTEND;
 
 	return rx_offload_capa;
 }
