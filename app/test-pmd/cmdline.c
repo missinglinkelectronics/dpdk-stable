37a858065942f9b36b1a68a6ede7611489bbdcea
Chengchang Tang <tangchengchang@huawei.com>
2020-09-25 20:47:14 +0800

    app/testpmd: fix port id check in Tx VLAN command
    [ upstream commit b428e71775e43acc7b074c82b0d9e08f401dde6b ]
    
    To set Tx vlan offloads, it is required to stop port firstly. But before
    checking whether the port is stopped, the port id entered by the user
    is not checked for validity. When the port id is illegal, it would lead
    to a segmentation fault since it attempts to access a member of
    non-existent port.
    
    This patch adds verification of port id in tx vlan offloads and remove
    duplicated check.
    
    Fixes: 597f9fafe13b ("app/testpmd: convert to new Tx offloads API")
    
    Signed-off-by: Chengchang Tang <tangchengchang@huawei.com>
    Signed-off-by: Wei Hu (Xavier) <xavier.huwei@huawei.com>
    Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>

--- app/test-pmd/cmdline.c
+++ app/test-pmd/cmdline.c
@@ -4000,6 +4000,9 @@ cmd_tx_vlan_set_parsed(void *parsed_result,
 {
 	struct cmd_tx_vlan_set_result *res = parsed_result;
 
+	if (port_id_is_invalid(res->port_id, ENABLED_WARN))
+		return;
+
 	if (!port_is_stopped(res->port_id)) {
 		printf("Please stop port %d first\n", res->port_id);
 		return;
@@ -4053,6 +4056,9 @@ cmd_tx_vlan_set_qinq_parsed(void *parsed_result,
 			    __attribute__((unused)) void *data)
 {
 	struct cmd_tx_vlan_set_qinq_result *res = parsed_result;
+
+	if (port_id_is_invalid(res->port_id, ENABLED_WARN))
+		return;
 
 	if (!port_is_stopped(res->port_id)) {
 		printf("Please stop port %d first\n", res->port_id);
@@ -4166,6 +4172,9 @@ cmd_tx_vlan_reset_parsed(void *parsed_result,
 			 __attribute__((unused)) void *data)
 {
 	struct cmd_tx_vlan_reset_result *res = parsed_result;
+
+	if (port_id_is_invalid(res->port_id, ENABLED_WARN))
+		return;
 
 	if (!port_is_stopped(res->port_id)) {
 		printf("Please stop port %d first\n", res->port_id);
