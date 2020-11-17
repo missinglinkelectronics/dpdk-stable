834827ae2a6e6bf38a8dc93da6d0cd8d63c6d858
Gregory Etelson <getelson@nvidia.com>
2020-11-17 20:49:39 +0200

    examples/l3fwd-power: check packet types after start
    [ upstream commit 6184a02223722b929ae99dff2ba967626448dd63 ]
    
    l3fwd-power uses `--parse-ptype' parameter to query egress packets
    type.  Before that feature is enabled l3fwd-power verifies PMD ability
    to advertise supported packet types with
    rte_eth_dev_get_supported_ptypes().
    
    The above ethdev function API contains this note:
    "
    Better to invoke this API after the device is already started or rx burst
    function is decided, to obtain correct supported ptypes.
    "
    
    The patch updates code for PMDs that register supported types after
    dev_start.
    
    Fixes: 82bea4661626 ("examples/l3fwd-power: add --parse-ptype option")
    
    Signed-off-by: Gregory Etelson <getelson@nvidia.com>

--- examples/l3fwd-power/main.c
+++ examples/l3fwd-power/main.c
@@ -2149,9 +2149,7 @@ main(int argc, char **argv)
 				if (add_cb_parse_ptype(portid, queueid) < 0)
 					rte_exit(EXIT_FAILURE,
 						 "Fail to add ptype cb\n");
-			} else if (!check_ptype(portid))
-				rte_exit(EXIT_FAILURE,
-					 "PMD can not provide needed ptypes\n");
+			}
 		}
 	}
 
@@ -2177,6 +2175,11 @@ main(int argc, char **argv)
 			rte_eth_promiscuous_enable(portid);
 		/* initialize spinlock for each port */
 		rte_spinlock_init(&(locks[portid]));
+
+		if (!parse_ptype)
+			if (!check_ptype(portid))
+				rte_exit(EXIT_FAILURE,
+					"PMD can not provide needed ptypes\n");
 	}
 
 	check_all_ports_link_status(enabled_port_mask);
