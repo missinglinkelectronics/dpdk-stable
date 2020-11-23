ovsrobot/dpdk

distributor: fix return pkt calls in single mode
In the single legacy version of the distributor synchronization
requires continues exchange of buffers between distributor
and workers. Empty buffers are sent if only handshake
synchronization is required.
However calls to the rte_distributor_return_pkt()
with 0 buffers in single mode were ignored and not passed to the
legacy algorithm implementation causing lack of synchronization.

This patch fixes this issue by passing NULL as buffer which is
a valid way of sending just synchronization handshakes
in single mode.

Fixes: 775003ad2f96 ("distributor: add new burst-capable library")
Cc: david.hunt@intel.com
Cc: stable@dpdk.org

Signed-off-by: Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
Acked-by: David Hunt <david.hunt@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/librte_distributor/rte_distributor.c
@@ -167,6 +167,9 @@ rte_distributor_return_pkt(struct rte_distributor *d,
 		if (num == 1)
 			return rte_distributor_return_pkt_single(d->d_single,
 				worker_id, oldpkt[0]);
+		else if (num == 0)
+			return rte_distributor_return_pkt_single(d->d_single,
+				worker_id, NULL);
 		else
 			return -EINVAL;
 	}

missinglinkelectronics/dpdk-stable

[ upstream commit f25fe0d5e3e0dd1650845eca928f2263898f5a19 ]

lib/librte_distributor/rte_distributor.c
@@ -181,6 +181,9 @@ rte_distributor_return_pkt_v1705(struct rte_distributor *d,
 		if (num == 1)
 			return rte_distributor_return_pkt_v20(d->d_v20,
 				worker_id, oldpkt[0]);
+		else if (num == 0)
+			return rte_distributor_return_pkt_v20(d->d_v20,
+				worker_id, NULL);
 		else
 			return -EINVAL;
 	}