3aa1d65232410e17f12fb7cd8fd76fc612e117df
Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
2020-12-01 09:09:27 +0530

    net/bnxt: fix UDP tunnel port removal
    [ upstream commit c4ae7adafa23fb1121b96518a9b9b815288e680c ]
    
    The HWRM supports only one global destination port for a tunnel type.
    When port is stopped, driver deletes the UDP tunnel port configured
    in the HW, but it does not update the counter which causes the
    tunnel port addition to fail after port is started again.
    
    Fixed to update the counter when tunnel port is deleted.
    
    Fixes: 10d074b2022d ("net/bnxt: support tunneling")
    
    Signed-off-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Reviewed-by: Ajit Khaparde <ajit.khaparde@broadcom.com>
    Reviewed-by: Somnath Kotur <somnath.kotur@broadcom.com>

--- drivers/net/bnxt/bnxt_hwrm.c
+++ drivers/net/bnxt/bnxt_hwrm.c
@@ -2113,11 +2113,10 @@ void bnxt_free_tunnel_ports(struct bnxt *bp)
 	if (bp->vxlan_port_cnt)
 		bnxt_hwrm_tunnel_dst_port_free(bp, bp->vxlan_fw_dst_port_id,
 			HWRM_TUNNEL_DST_PORT_FREE_INPUT_TUNNEL_TYPE_VXLAN);
-	bp->vxlan_port = 0;
+
 	if (bp->geneve_port_cnt)
 		bnxt_hwrm_tunnel_dst_port_free(bp, bp->geneve_fw_dst_port_id,
 			HWRM_TUNNEL_DST_PORT_FREE_INPUT_TUNNEL_TYPE_GENEVE);
-	bp->geneve_port = 0;
 }
 
 void bnxt_free_all_hwrm_resources(struct bnxt *bp)
@@ -2909,6 +2908,18 @@ int bnxt_hwrm_tunnel_dst_port_free(struct bnxt *bp, uint16_t port,
 
 	HWRM_CHECK_RESULT();
 	HWRM_UNLOCK();
+
+	if (tunnel_type ==
+	    HWRM_TUNNEL_DST_PORT_FREE_INPUT_TUNNEL_TYPE_VXLAN) {
+		bp->vxlan_port = 0;
+		bp->vxlan_port_cnt = 0;
+	}
+
+	if (tunnel_type ==
+	    HWRM_TUNNEL_DST_PORT_FREE_INPUT_TUNNEL_TYPE_GENEVE) {
+		bp->geneve_port = 0;
+		bp->geneve_port_cnt = 0;
+	}
 
 	return rc;
 }
