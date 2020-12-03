missinglinkelectronics/dpdk-stable

commit a7500412fc5c9c1b260da73a163dda9f064cdcfe

examples/multi_process: fix build on Ubuntu 20.04
[ upstream commit 27b549c12df2ef2db6b271795b4df7b14a2d9c2c ]

Two warnings are reported by gcc 9.3.0 on Ubuntu 20.04.

When producing a printable mac address the buffer was appropriately sized
for holding the mac address exactly, but the actual snprintf included a
'\n' character at the end, which means that the snprintf technically is
getting truncated i.e. the \n would not be added due to lack of space.
This gets flagged as a problem by modern versions of gcc, e.g. on Ubuntu
20.04.

main.c:77:37: warning: ‘__builtin___snprintf_chk’ output truncated
  before the last format character [-Wformat-truncation=]
   77 |     "%02x:%02x:%02x:%02x:%02x:%02x\n",
      |                                     ^

Since the \n is getting stripped anyway, we can fix the issue by just
removing it. In the process we can switch to using the standard ethernet
address formatting function from rte_ether.h.

The other warning is about possible string truncation when getting the
RX queue name:

In file included from init.c:36:
init.c: In function ‘init’:
../shared/common.h:38:28: warning: ‘%u’ directive output may be truncated
  writing between 1 and 10 bytes into a region of size 8
  [-Wformat-truncation=]
   38 | #define MP_CLIENT_RXQ_NAME "MProc_Client_%u_RX"
      |                            ^~~~~~~~~~~~~~~~~~~~
../shared/common.h:52:35: note: in expansion of macro ‘MP_CLIENT_RXQ_NAME’
   52 |  snprintf(buffer, sizeof(buffer), MP_CLIENT_RXQ_NAME, id);
      |                                   ^~~~~~~~~~~~~~~~~~

This is a false positive, as the value of the "id" is limited to 255,
being stored in the app as a uint8_t value, removing the possibility of
the %u being replaced by anything other then 3 characters max (rather than
up to 10 as thought by the compiler). Therefore, the warning can be easily
removed by changing the type of the "id" parameter to the local function

examples/multi_process/client_server_mp/mp_server/main.c
@@ -59,18 +59,21 @@ static struct client_rx_buf *cl_rx_buf;
 static const char *
 get_printable_mac_addr(uint16_t port)
 {
-	static const char err_address[] = "00:00:00:00:00:00";
-	static char addresses[RTE_MAX_ETHPORTS][sizeof(err_address)];
+	static const struct ether_addr null_mac; /* static defaults to 0 */
+	static char err_address[32];
+	static char addresses[RTE_MAX_ETHPORTS][32];
 
-	if (unlikely(port >= RTE_MAX_ETHPORTS))
+	if (unlikely(port >= RTE_MAX_ETHPORTS)) {
+		if (err_address[0] == '\0')
+			rte_ether_format_addr(err_address,
+					sizeof(err_address), &null_mac);
 		return err_address;
+	}
 	if (unlikely(addresses[port][0]=='\0')){
 		struct ether_addr mac;
 		rte_eth_macaddr_get(port, &mac);
-		snprintf(addresses[port], sizeof(addresses[port]),
-				"%02x:%02x:%02x:%02x:%02x:%02x\n",
-				mac.addr_bytes[0], mac.addr_bytes[1], mac.addr_bytes[2],
-				mac.addr_bytes[3], mac.addr_bytes[4], mac.addr_bytes[5]);
+		rte_ether_format_addr(addresses[port],
+				sizeof(addresses[port]), &mac);
 	}
 	return addresses[port];
 }

examples/multi_process/client_server_mp/shared/common.h
@@ -43,7 +43,7 @@ struct port_info {
  * Given the rx queue name template above, get the queue name
  */
 static inline const char *
-get_rx_queue_name(unsigned id)
+get_rx_queue_name(uint8_t id)
 {
 	/* buffer for return value. Size calculated by %u being replaced
 	 * by maximum 3 digits (plus an extra byte for safety) */

commit 69b4afdf8c362be6f5a8bc7288fbc21d453ea25b

examples/multi_process: fix compilation
Fix compilation of examples/multi_process:

> examples/multi_process/client_server_mp/mp_server/main.c:68:4: error:
> implicit declaration of function ‘rte_ether_format_addr’;
> did you mean ‘ether_format_addr’? [-Werror=implicit-function-declaration]
>     rte_ether_format_addr(err_address,
>     ^~~~~~~~~~~~~~~~~~~~~

This failure happens after the backport of
commit a7500412fc5c ("examples/multi_process: fix build on Ubuntu
20.04"), which uses a function that does not exist in this branch. The
function was introduced by upstream commit 538da7a1cad2 ("net: add rte
prefix to ether functions"), and is just a renaming of an existing
function.

Fixes: a7500412fc5c ("examples/multi_process: fix build on Ubuntu 20.04")

Signed-off-by: Olivier Matz <olivier.matz@6wind.com>

examples/multi_process/client_server_mp/mp_server/main.c
@@ -65,14 +65,14 @@ get_printable_mac_addr(uint16_t port)
 
 	if (unlikely(port >= RTE_MAX_ETHPORTS)) {
 		if (err_address[0] == '\0')
-			rte_ether_format_addr(err_address,
+			ether_format_addr(err_address,
 					sizeof(err_address), &null_mac);
 		return err_address;
 	}
 	if (unlikely(addresses[port][0]=='\0')){
 		struct ether_addr mac;
 		rte_eth_macaddr_get(port, &mac);
-		rte_ether_format_addr(addresses[port],
+		ether_format_addr(addresses[port],
 				sizeof(addresses[port]), &mac);
 	}
 	return addresses[port];