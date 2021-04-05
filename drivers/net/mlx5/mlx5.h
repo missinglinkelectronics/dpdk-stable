b0038ab92677cf1503f1b107f9768cc4385abc30
Viacheslav Ovsiienko <viacheslavo@nvidia.com>
2021-04-05 10:07:16 +0000

    net/mlx5: remove drop queue function prototypes
    [ upstream commit a4af5eed4091f74c10381f1e5d5ec1a058f19a9a ]
    
    There are some leftovers of removed code - there are
    no drop queue handling routines anymore.
    
    Fixes: 78be885295b8 ("net/mlx5: handle drop queues as regular queues")
    
    Signed-off-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>

--- drivers/net/mlx5/mlx5.h
+++ drivers/net/mlx5/mlx5.h
@@ -966,8 +966,6 @@ int mlx5_ctrl_flow(struct rte_eth_dev *dev,
 		   struct rte_flow_item_eth *eth_spec,
 		   struct rte_flow_item_eth *eth_mask);
 struct rte_flow *mlx5_flow_create_esw_table_zero_flow(struct rte_eth_dev *dev);
-int mlx5_flow_create_drop_queue(struct rte_eth_dev *dev);
-void mlx5_flow_delete_drop_queue(struct rte_eth_dev *dev);
 void mlx5_flow_async_pool_query_handle(struct mlx5_ibv_shared *sh,
 				       uint64_t async_id, int status);
 void mlx5_set_query_alarm(struct mlx5_ibv_shared *sh);
