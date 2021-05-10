8679bdcfadd170ed998b81f60114f5c81b236f75
Thomas Monjalon <thomas@monjalon.net>
2021-05-10 22:45:37 +0200

    doc: fix build with Sphinx 4
    [ upstream commit 86ff0663639417f9d25907d09343a5c231e32a51 ]
    
    Sphinx 4.0 became stricter with permalink configuration:
    "
    html_add_permalinks has been deprecated since v3.5.0.
    Please use html_permalinks and html_permalinks_icon instead.
    "
    
    The new variable is used while keeping compatibility
    with older Sphinx versions.
    
    Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>
    Reviewed-by: David Marchand <david.marchand@redhat.com>

--- doc/guides/conf.py
+++ doc/guides/conf.py
@@ -33,7 +33,10 @@ except:
 project = 'Data Plane Development Kit'
 html_logo = '../logo/DPDK_logo_vertical_rev_small.png'
 latex_logo = '../logo/DPDK_logo_horizontal_tag.png'
-html_add_permalinks = ""
+if LooseVersion(sphinx_version) >= LooseVersion('3.5'):
+    html_permalinks = False
+else:
+    html_add_permalinks = ""
 html_show_copyright = False
 highlight_language = 'none'
 
