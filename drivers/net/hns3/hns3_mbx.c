DPDK/dpdk

net/hns3: fix mailbox error message
The hns3_dev_handle_mbx_msg() could be called under both PF and VF,
but the error messages show VF.

Fixes: 109e4dd1bd7a ("net/hns3: get link state change through mailbox")
Cc: stable@dpdk.org

Signed-off-by: Chengwen Feng <fengchengwen@huawei.com>
Signed-off-by: Min Hu (Connor) <humin29@huawei.com>

drivers/net/hns3/hns3_mbx.c
@@ -528,8 +528,7 @@ hns3_dev_handle_mbx_msg(struct hns3_hw *hw)
 			hns3_handle_promisc_info(hw, req->msg[1]);
 			break;
 		default:
-			hns3_err(hw,
-				 "VF received unsupported(%u) mbx msg from PF",
+			hns3_err(hw, "received unsupported(%u) mbx msg",
 				 req->msg[0]);
 			break;
 		}

missinglinkelectronics/dpdk-stable

[ upstream commit 871aa63542cedab7b920f2dd54fc3dc8ef82f3ca ]

drivers/net/hns3/hns3_mbx.c
@@ -386,8 +386,7 @@ hns3_dev_handle_mbx_msg(struct hns3_hw *hw)
 			hns3_handle_link_change_event(hw, req);
 			break;
 		default:
-			hns3_err(hw,
-				 "VF received unsupported(%d) mbx msg from PF",
+			hns3_err(hw, "received unsupported(%d) mbx msg",
 				 req->msg[0]);
 			break;
 		}