e3f1ad1116f8d6bfd2bb32c3ac0e27c67495c29d
Bing Zhao <bingz@nvidia.com>
2021-11-05 08:10:57 +0200

    net/mlx5: fix RETA update without stopping device
    [ upstream commit 6b5b3005cb1854e471fa1ef3eb579dbd444c05b0 ]
    
    The global redirection table is used to create the default flow
    rules for the ingress traffic with the lowest priority. It is also
    used to create the default RSS rule in the destination table when
    there is a tunnel offload.
    
    To update the RETA in-flight, there is no restriction in the ethdev
    API. In the previous implementation of mlx5, a port restart was
    needed to make the new configuration take effect.
    
    The restart is heavy, e.g., all the queues will be released and
    reallocated, users' rules will be flushed. Since the restart is
    internal, there is a risk to crash the application when some change
    in the ethdev is introduced but no workaround is done in mlx5 PMD.
    
    The users' rules, including the default miss rule for tunnel
    offload, should not be impacted by the RETA update. It is improper
    to flush all rules when updating RETA.
    
    With this patch, only the default rules will be flushed and
    re-created with the new table configuration.
    
    Fixes: 3f2fe392bd49 ("net/mlx5: fix crash during RETA update")
    
    Signed-off-by: Bing Zhao <bingz@nvidia.com>
    Acked-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>

--- drivers/net/mlx5/mlx5_rss.c
+++ drivers/net/mlx5/mlx5_rss.c
@@ -221,12 +221,6 @@ mlx5_dev_rss_reta_update(struct rte_eth_dev *dev,
 		assert(reta_conf[idx].reta[pos] < priv->rxqs_n);
 		(*priv->reta_idx)[i] = reta_conf[idx].reta[pos];
 	}
-
 	priv->skip_default_rss_reta = 1;
-
-	if (dev->data->dev_started) {
-		mlx5_dev_stop(dev);
-		return mlx5_dev_start(dev);
-	}
-	return 0;
+	return mlx5_traffic_restart(dev);
 }
