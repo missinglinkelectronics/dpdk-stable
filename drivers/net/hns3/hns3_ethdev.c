21f145ed527e007d18cc825f59ad773f43545bfb
Huisong Li <lihuisong@huawei.com>
2021-12-25 18:53:43 +0800

    net/hns3: fix multi-process action register and unregister
    [ upstream commit 841f8693536f9410fd51d385e1090d35cfe59914 ]
    
    The multi-process has the following problems:
    1) After a port in primary process is closed, the mp action of the
       process is unregistered. Which will cause that other device in the
       primary process cannot respond to requests from secondary processes.
    2) Because variable "hns3_inited" is set to true without returning an
       initial value, the mp action cannot be registered again after it is
       unregistered.
    3) The mp action of primary and secondary process need to be registered
       only once regardless of port numbers in the process. That's what
       variable "hns3_inited" does. But the variable is difficult to
       understand.
    
    This patch adds a hns3_process_local_data structure to resolve above
    problems.
    
    Fixes: 9570b1fdbdad ("net/hns3: check multi-process action register result")
    Fixes: 23d4b61fee5d ("net/hns3: support multiple process")
    
    Signed-off-by: Huisong Li <lihuisong@huawei.com>
    Signed-off-by: Min Hu (Connor) <humin29@huawei.com>

--- drivers/net/hns3/hns3_ethdev.c
+++ drivers/net/hns3/hns3_ethdev.c
@@ -5439,6 +5439,7 @@ hns3_dev_init(struct rte_eth_dev *eth_dev)
 			goto err_mp_init_secondary;
 		}
 		__atomic_fetch_add(&hw->secondary_cnt, 1, __ATOMIC_RELAXED);
+		process_data.eth_dev_cnt++;
 		return 0;
 	}
 
@@ -5449,6 +5450,7 @@ hns3_dev_init(struct rte_eth_dev *eth_dev)
 			     ret);
 		goto err_mp_init_primary;
 	}
+	process_data.eth_dev_cnt++;
 
 	hw->adapter_state = HNS3_NIC_UNINITIALIZED;
 
