DPDK/dpdk

commit 7b5609a5a0e790f4d51ac19dc937a9c0ef203f5b

cryptodev: fix RSA key type name
[ upstream commit 3387912f5cacd934594f2c7a0c34eda7279419a8 ]

This patch fixes misspelled RTE_RSA_KEY_TYPE_QT,
this will prevent checkpach from complaining wherever
change to RSA is being made.

Fixes: 26008aaed14c ("cryptodev: add asymmetric xform and op definitions")

Signed-off-by: Arek Kusztal <arkadiuszx.kusztal@intel.com>
Acked-by: Akhil Goyal <gakhil@marvell.com>

lib/librte_cryptodev/rte_crypto_asym.h
@@ -146,7 +146,7 @@ enum rte_crypto_rsa_padding_type {
 enum rte_crypto_rsa_priv_key_type {
 	RTE_RSA_KEY_TYPE_EXP,
 	/**< RSA private key is an exponent */
-	RTE_RSA_KET_TYPE_QT,
+	RTE_RSA_KEY_TYPE_QT,
 	/**< RSA private key is in quintuple format
 	 * See rte_crypto_rsa_priv_key_qt
 	 */

app/test/test_cryptodev_asym.c
@@ -559,7 +559,7 @@ test_one_case(const void *test_case, int sessionless)
 						status = test_cryptodev_asym_op(
 							&testsuite_params,
 							&tc, test_msg, sessionless, i,
-							RTE_RSA_KET_TYPE_QT);
+							RTE_RSA_KEY_TYPE_QT);
 					}
 					if (status)
 						break;

app/test/test_cryptodev_rsa_test_vectors.h
@@ -378,7 +378,7 @@ struct rte_crypto_asym_xform rsa_xform_crt = {
 			.data = rsa_e,
 			.length = sizeof(rsa_e)
 		},
-		.key_type = RTE_RSA_KET_TYPE_QT,
+		.key_type = RTE_RSA_KEY_TYPE_QT,
 		.qt = {
 			.p = {
 				.data = rsa_p,

drivers/crypto/qat/qat_asym.c
@@ -97,7 +97,7 @@ static void qat_clear_arrays_by_alg(struct qat_asym_op_cookie *cookie,
 		qat_clear_arrays(cookie, QAT_ASYM_MODINV_NUM_IN_PARAMS,
 				QAT_ASYM_MODINV_NUM_OUT_PARAMS, alg_size);
 	else if (xform->xform_type == RTE_CRYPTO_ASYM_XFORM_RSA) {
-		if (xform->rsa.key_type == RTE_RSA_KET_TYPE_QT)
+		if (xform->rsa.key_type == RTE_RSA_KEY_TYPE_QT)
 			qat_clear_arrays_crt(cookie, alg_size);
 		else {
 			qat_clear_arrays(cookie, QAT_ASYM_RSA_NUM_IN_PARAMS,
@@ -370,7 +370,7 @@ qat_asym_fill_arrays(struct rte_crypto_asym_op *asym_op,
 					return -(EINVAL);
 				}
 			}
-			if (xform->rsa.key_type == RTE_RSA_KET_TYPE_QT) {
+			if (xform->rsa.key_type == RTE_RSA_KEY_TYPE_QT) {
 
 				qat_req->input_param_count =
 						QAT_ASYM_RSA_QT_NUM_IN_PARAMS;

missinglinkelectronics/dpdk-stable

commit 1566e521c3dac7e301737b775964a54a700ee6b7

cryptodev: add backward-compatible enum
The enum was renamed to fix a typo in main, and backported.
But in the stable release we want to keep backward compatibility
when possible, so also define the old name so that both old and
new code will work.

Fixes: 7b5609a5a0e7 ("cryptodev: fix RSA key type name")

Signed-off-by: Luca Boccassi <bluca@debian.org>

lib/librte_cryptodev/rte_crypto_asym.h
@@ -137,6 +137,8 @@ enum rte_crypto_rsa_priv_key_type {
 	/**< RSA private key is in quintuple format
 	 * See rte_crypto_rsa_priv_key_qt
 	 */
+	RTE_RSA_KET_TYPE_QT = RTE_RSA_KEY_TYPE_QT,
+	/**< Backward-compatible definition of old name */
 };
 
 /**