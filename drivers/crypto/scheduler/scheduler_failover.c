DPDK/dpdk

crypto/scheduler: fix queue pair in scheduler failover
This commit fixes wrong qp_id value in cryptodev scheduler in failover
mode.

Fixes: 37f075dad1e9 ("crypto/scheduler: add fail-over scheduling mode")
Cc: stable@dpdk.org

Signed-off-by: Jakub Wysocki <jakubx.wysocki@intel.com>
Acked-by: Fan Zhang <roy.fan.zhang@intel.com>

drivers/crypto/scheduler/scheduler_failover.c
@@ -157,6 +157,9 @@ scheduler_start(struct rte_cryptodev *dev)
 			((struct scheduler_qp_ctx *)
 				dev->data->queue_pairs[i])->private_qp_ctx;
 
+		sched_ctx->workers[PRIMARY_WORKER_IDX].qp_id = i;
+		sched_ctx->workers[SECONDARY_WORKER_IDX].qp_id = i;
+
 		rte_memcpy(&qp_ctx->primary_worker,
 				&sched_ctx->workers[PRIMARY_WORKER_IDX],
 				sizeof(struct scheduler_worker));

missinglinkelectronics/dpdk-stable

[ upstream commit b302708673017b560644b954ed78ef6cdba1aa2d ]

drivers/crypto/scheduler/scheduler_failover.c
@@ -156,6 +156,9 @@ scheduler_start(struct rte_cryptodev *dev)
 			((struct scheduler_qp_ctx *)
 				dev->data->queue_pairs[i])->private_qp_ctx;
 
+		sched_ctx->slaves[PRIMARY_SLAVE_IDX].qp_id = i;
+		sched_ctx->slaves[SECONDARY_SLAVE_IDX].qp_id = i;
+
 		rte_memcpy(&qp_ctx->primary_slave,
 				&sched_ctx->slaves[PRIMARY_SLAVE_IDX],
 				sizeof(struct scheduler_slave));