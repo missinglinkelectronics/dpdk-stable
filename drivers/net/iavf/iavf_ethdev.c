19ba437a1358cd964c83bfba919435a2c1f3dbda
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2022-08-03 14:17:43 +0200

    Revert "net/iavf: fix Rx queue interrupt setting"
    This reverts commit cc3e9347e0a89a09f0980b44327b47fd9009e72c.
    It causes a build fail:
    In file included from ../drivers/net/iavf/iavf_ethdev.c:13:
    ../drivers/net/iavf/iavf_ethdev.c: In function ‘iavf_dev_rx_queue_intr_disable’:
    ../drivers/net/iavf/iavf_ethdev.c:1212:9: error:
     ‘IAVF_VFINT_DYN_CTLN1_WB_ON_ITR_MASK’ undeclared (first use in this function);
      did you mean ‘IAVFINT_DYN_CTLN1_WB_ON_ITR_MASK’?
     1212 |         IAVF_VFINT_DYN_CTLN1_WB_ON_ITR_MASK);
    
    It missed (parts of) commit 89214fe9 "net/iavf/base: move to drivers
    common directory"

--- drivers/net/iavf/iavf_ethdev.c
+++ drivers/net/iavf/iavf_ethdev.c
@@ -1209,7 +1209,7 @@ iavf_dev_rx_queue_intr_disable(struct rte_eth_dev *dev, uint16_t queue_id)
 
 	IAVF_WRITE_REG(hw,
 		      IAVFINT_DYN_CTLN1(msix_intr - IAVF_RX_VEC_START),
-		      IAVF_VFINT_DYN_CTLN1_WB_ON_ITR_MASK);
+		      0);
 
 	IAVF_WRITE_FLUSH(hw);
 	return 0;
